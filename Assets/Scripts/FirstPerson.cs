5a4dabebf695f3fd6b39e2a1cea9aa76e2aaf086
loretulip <lorenasvez@gmail.com>
2024-10-28 17:13:43 +0100

    Avance 28/10

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class FirstPerson : MonoBehaviour
 {
+    [Header ("Movimiento")]
     [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float escalaGravedad;
+    [SerializeField] private float alturaSalto;
+
+    [Header("Deteccion Suelo")]
+    [SerializeField] private Transform pies;
+    [SerializeField] private LayerMask queEsSuelo;
+    [SerializeField] private float radio;
+
     CharacterController controller;
     private Camera cam;
 
-
+    private Vector3 movimientoVertical;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState=CursorLockMode.Locked;
         controller = GetComponent<CharacterController>();
         cam = Camera.main;
     }
@@ -28,13 +38,43 @@ public class FirstPerson : MonoBehaviour
         //Si existe input...
         if (input.sqrMagnitude > 0) 
         {
-            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
             transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
 
             Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
             controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+            
+        }        
+        AplicarGravedad();
+        DeteccionSuelo();
+    }
 
+    private void AplicarGravedad()
+    {
+        //Mi movimiento vertical en la y va aumentándose a cada cierta escala por segundo
+        movimientoVertical.y += escalaGravedad * Time.deltaTime;
+        controller.Move(movimientoVertical * Time.deltaTime);
+    }
+    private void DeteccionSuelo()
+    {
+        //Tengo que lanzar una bola de detección en mis pies para detectar si hay suelo
+        Collider[] collsDetectados = Physics.OverlapSphere(pies.position, radio, queEsSuelo);
+        if(collsDetectados.Length > 0)
+        {
+            movimientoVertical.y = 0;
+            Saltar();
         }
-
+    }
+    //Para dibujar cualquier figura en la escena
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pies.position, radio);
+    }
+    private void Saltar()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movimientoVertical.y = Mathf.Sqrt(-2 * escalaGravedad * alturaSalto);
+        }
     }
 }
