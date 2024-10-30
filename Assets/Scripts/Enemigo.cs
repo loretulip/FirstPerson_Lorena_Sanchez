892bd0e472be059085d4e6bab47670ccb0550818
loretulip <lorenasvez@gmail.com>
2024-10-30 20:03:24 +0100

    Avance 30/10

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemigo : MonoBehaviour
+{
+    //private FirstPerson player;
+    // Start is called before the first frame update
+    //private NavMeshAgent agent;
+    void Start()
+    {
+        //agent = GetComponent<NavMeshAgent>();
+       // player = GameObject.FindObjectOfType<FirstPerson>; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //agent.SetDestination(player.transform.position);
+    }
+}
