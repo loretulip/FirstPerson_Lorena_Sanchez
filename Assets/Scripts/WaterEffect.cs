82eba31fcee8c1d01415246c1f8d59933e31fba9
loretulip <lorenasvez@gmail.com>
2024-11-25 18:20:42 +0100

    Water effect y sonido 25/11

--- Assets/Scripts/WaterEffect.cs
+++ Assets/Scripts/WaterEffect.cs
@@ -6,7 +6,10 @@ using UnityEngine.Rendering.Universal;
 
 public class WaterEffect : MonoBehaviour
 {
+    [SerializeField] private float velocidad;
     private Volume efecto;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +19,18 @@ public class WaterEffect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Time.deltaTime = Tiempo entre dos frames consecutivos
+        // Time.time = Tiempo de juego total
+        // Debug.Log(Mathf.Cos(velocidad * Time.time));
+
         // Busca en tu profeile si tienes el efecto LensDistortion
         if (efecto.profile.TryGet(out LensDistortion distortion))
         {
-           // distortion.xMultiplier;
+            // Es como: Vector3 sitio = new Vector3 (0, 0, 0);  
+            FloatParameter xValue = new FloatParameter(1 + Mathf.Cos(velocidad * Time.time) / 2);
+            FloatParameter yValue = new FloatParameter(1 + Mathf.Sin(velocidad * Time.time) / 2);
+           distortion.xMultiplier.SetValue(xValue);
+           distortion.yMultiplier.SetValue(yValue);
         }
     }
 }
